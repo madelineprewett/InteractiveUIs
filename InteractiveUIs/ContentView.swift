87092ed84dfa16dadfeb49675236ed802f1129a5
Madeline Prewett <madsprewett@gmail.com>
2023-07-13 12:57:49 -0700

    commented hey

--- InteractiveUIs/ContentView.swift
+++ InteractiveUIs/ContentView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+        var body: some View {
+            
+            
+            
+            VStack {
+                Text(textTitle)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                TextField("Type name here...", text: $name)
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                    .border(Color.gray, width: 1)
+                
+                Button("Submit") {
+                    textTitle = "Welcome \(name)!"
+                    print(name)
+                }
+                .font(.title)
+                .fontWeight(.semibold)
+                .buttonStyle(.borderedProminent)
+                .tint(.pink)
+                
+            }
+            .padding()
         }
-        .padding()
     }
-}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//hey
