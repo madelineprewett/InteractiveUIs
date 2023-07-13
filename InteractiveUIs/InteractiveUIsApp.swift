e04545bf0ebb22f08750b38b7b2e8a5aaecd7cde
Madeline Prewett <madsprewett@gmail.com>
2023-07-13 11:46:47 -0700

    Initial Commit

---
+++ InteractiveUIs/InteractiveUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIsApp.swift
+//  InteractiveUIs
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
